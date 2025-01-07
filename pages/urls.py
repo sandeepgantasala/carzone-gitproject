611a403c54d9682b11a465f79b330ce367a9969d
Sandeep Gantasala <sandeepgantasal190@gmail.com>
2025-01-07 14:45:18 -0700

    Added pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("",views.home, name="home"),
+    path("about",views.about,name="about"),
+    path("service",views.service,name="service"),
+    path("contact",views.contact,name="contact"),
+    path("search",views.search,name="search"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render( request,"pages/home.html")
+
+def about(request):
+    return render( request,"pages/about.html")
+
+def service(request):
+    return render(request,"pages/service.html")
+
+def contact(request):
+    return render(request, "pages/contact.html")
+
+def search(request):
+    return render(request,"pages/search.html")

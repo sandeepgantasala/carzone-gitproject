51dcece3330ec74f54a9b13326a821690a0c1176
Sandeep Gantasala <sandeepgantasala@Sandeeps-MacBook-Air.local>
2025-02-27 10:18:41 -0700

    Add login/logout to user

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,20 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def login(request):
-    return render(request,"accounts/login.html")
+    if request.method =="POST":
+        username= request.POST["username"]
+        password= request.POST["password"]
+
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,"You are now logged in")
+            return redirect("dashboard")
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect("login")
+    else:
+        return render(request,"accounts/login.html")
 
 def register(request):
     if request.method == "POST":
@@ -39,6 +52,10 @@ def register(request):
         return render(request,"accounts/register.html")
 
 def logout(request):
+    if request.method =="POST":
+        auth.logout(request)
+        messages.success(request,"You are now logged out")
+        return redirect("login")
     return redirect("home")
 
 def dashboard(request):
